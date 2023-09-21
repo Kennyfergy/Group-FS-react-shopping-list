de9922c5bc190a93c4eae86665f0068ec1a61e49
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-21 14:56:34 -0500

    webpage is displaying without bugs, need to hook up buttons

--- src/components/ShoppingListItem/ShoppingListItem.jsx
+++ src/components/ShoppingListItem/ShoppingListItem.jsx
@@ -1,9 +1,14 @@
 export default function ShoppingListItem({ item }) {
-    return(
-        <tr>
-            <td>{item.name}</td>
-            <td>{item.qunatity} {item.unit}</td>
-            <td><button type="buy">Buy</button><button type="delete">Remove</button></td>
-        </tr>
-    )
+  return (
+    <tr>
+      <td>{item.name}</td>
+      <td>
+        {item.quantity} {item.unit}
+      </td>
+      <td>
+        <button type="buy">Buy</button>
+        <button type="delete">Remove</button>
+      </td>
+    </tr>
+  );
 }
