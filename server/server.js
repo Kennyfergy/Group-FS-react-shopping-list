fdb1508d2b79792e646ad371d60ff5a90f28fa06
jcolago <jcolago2005@gmail.com>
2023-09-21 18:58:36 -0500

    Added clearTable and resetItems functions in App.jsx

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 const itemsRouter = require("./routes/items.router");
 
-app.use(`/items`, itemsRouter);
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static("build"));
@@ -12,7 +12,7 @@ app.use(express.static("build"));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
-
+app.use(`/items`, itemsRouter);
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
   console.log("Listening on port: ", PORT);
