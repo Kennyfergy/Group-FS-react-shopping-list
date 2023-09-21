d5b78a682b2adcf48b3d7d87b5e662e72a8eba91
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-21 13:45:20 -0500

    updated names, may need to confirm names fof the value of my inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,47 @@
 import React from "react";
 
 export default function Form() {
-  const [item, setItem] = useState({ itemName: "" });
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    addItem(item);
-    clearInputFields();
-  };
+  //const [itemList, setItemList] = useState([]);
+  const [newItemName, setNewItemName] = useState("");
+  const [newItemQuantity, setNewItemQuantity] = useState("");
+  const [newItemUnit, setNewItemUnit] = useState("");
 
   const clearInputFields = () => {
-    setItem({ itemName: "" });
+    setNewItemName({ name: "" });
+    setNewItemQuantity({ quantity: "" });
+    setNewItemUnit({ unit: "" });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newItemName && newItemQuantity) {
+      addItem();
+      clearInputFields;
+    } else {
+      alert("The new item needs both a name and a quantity!");
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
       <input
-        onChange={(event) => setItem({ item: event.target.value })}
-        value={items.itemName}
+        onChange={(event) =>
+          setNewItemName({ newItemName: event.target.value })
+        }
+        value={setNewItemName.name}
         placeholder="Item Name"
+      />
+      <input
+        onChange={(event) =>
+          setNewItemQuantity({ newItemQuantity: event.target.value })
+        }
+        value={setNewItemQuantity.quantity}
+        placeholder="Quantity"
+      />
+      <input
+        onChange={(event) =>
+          setNewItemUnit({ newItemUnit: event.target.value })
+        }
+        value={setNewItemUnit.unit}
+        placeholder="Price"
       />
       <input type="submit" value="Submit" />
     </form>
